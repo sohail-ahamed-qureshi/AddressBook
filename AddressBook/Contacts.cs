6fc36227559fe5d6fad5201f36c6f2d61ca181f9
sohail-ahamed-qureshi <sohailqureshi82@gmail.com>
2021-06-03 13:16:51 +0530

    [sohail] Add: Saving Contact Details

---
+++ AddressBook/Contacts.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AddressBook
+{/// <summary>
+/// OOP's concepts for saving user details
+/// using Encapsulation, properties, Interface, Exceptions
+ /// getting contacts from user 
+ /// </summary>
+    interface IContactDetails
+    {
+        void ValidateContactDetails();
+    }
+    class Contacts : IContactDetails
+    {
+        //variables
+        readonly private string firstName;
+        readonly private string lastName;
+        readonly private string address;
+        readonly private string city;
+        readonly private string state;
+        readonly private int zipCode;
+        readonly private long phoneNumber;
+        readonly private string email;
+
+        //properties
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Address { get; set; }
+        public string City { get; set; }
+        public string State { get; set; }
+        public int ZipCode { get; set; }
+        public long PhoneNumber { get; set; }
+        public string Email { get; set; }
+
+        private bool ValidateFullName(string firstName, string lastName)
+        {
+            try
+            {
+                if (firstName != " " && lastName != " ")
+                    return true;
+                else
+                    return false;
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("Error in Full Name");
+            }
+            return false;
+        }
+
+        private bool ValidateAddress(string address, string city, string state, int zipCode)
+        {
+            try
+            {
+                if (address != " " && city != " " && state != " " && zipCode != 0)
+                    return true;
+                else
+                    return false;
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("Error in Address");
+            }
+            return false;
+        }
+        private bool ValidateContactNumbers(long phoneNumber, string email)
+        {
+            try
+            {
+                if (phoneNumber != 0 && email != " ")
+                    return true;
+                else
+                    return false;
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("Error in Contact Numbers");
+            }
+            return false;
+        }
+        public void ValidateContactDetails()
+        {
+            try
+            {
+                bool full_Name = ValidateFullName(FirstName, LastName);
+                bool addr = ValidateAddress(Address, City, State, ZipCode);
+                bool contact = ValidateContactNumbers(PhoneNumber, Email);
+                if (full_Name == true && addr == true && contact == true)
+                    Console.WriteLine("Contact Details Saved Successfully");
+                else
+                    Console.WriteLine("Contact Save Failed!!");
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("Error in Saving Details");
+            }
+
+        }
+    }
+}
--- AddressBook/Program.cs
+++ AddressBook/Program.cs
@@ -4,12 +4,30 @@ namespace AddressBook
 {
     class Program
     {/// <summary>
-    /// Address book program - displaying welcome message
+    /// Address book program - UC1 creating contact in addressbook
+    /// storing name address phone number email
     /// </summary>
     /// <param name="args"></param>
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, Welcome to Address book problem statement.");
+            Contacts contacts = new Contacts
+            {
+                FirstName = "Sohail",
+                LastName = "Ahamed",
+                Address = "SR nagar",
+                City = "Hospet",
+                State = "Karnataka",
+                ZipCode = 583201,
+                PhoneNumber = 1234567890,
+                Email = "sohailqureshi82@gmail.com"
+            };
+            contacts.ValidateContactDetails();
+            Console.WriteLine("Contact Details: ");
+            Console.WriteLine($"Full Name: {contacts.FirstName + contacts.LastName} ");
+            Console.WriteLine($"Phone Number: {contacts.PhoneNumber} ");
+            Console.WriteLine($"Address: {contacts.Address} \n \t{contacts.City} {contacts.State} \n \t{contacts.ZipCode} ");
+            Console.WriteLine($"Email: {contacts.Email} ");
+
         }
     }
 }
