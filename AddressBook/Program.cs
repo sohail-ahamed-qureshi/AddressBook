592336206783a35a140692a7647c281ff48413c7
sohail-ahamed-qureshi <sohailqureshi82@gmail.com>
2021-06-03 11:50:13 +0530

    [sohail] Add: Welcome message for Address book

---
+++ AddressBook/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace AddressBook
+{
+    class Program
+    {/// <summary>
+    /// Address book program - displaying welcome message
+    /// </summary>
+    /// <param name="args"></param>
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, Welcome to Address book problem statement.");
+        }
+    }
+}
